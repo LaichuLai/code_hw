818f11eb87b929392b22450502dde2045227c9d2
Laichu <cozzichilli@gmail.com>
2021-12-09 18:14:35 +0800

    update files

---
+++ ruby_practice/each_method_testing.rb
@@ -0,0 +1,9 @@
+稍早的問題是想複習有關params資料調度的一些觀念，所以自己亂參考一些網站的題目，然後就被考倒了
+
+lative_stock = [
+  {id: 1, price: 65.00, name: "StarWars_tshirt", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {id: 2, price: 29.99, name: "Micky_hat", quantity_by_size: {s: 1, xl: 4}},
+  {id: 3, price: 29.99, name: "IronMan_socks", quantity_by_size: {}]
+
+  #1.請設計出一個方法可以查詢不同產品以及對應尺寸的庫存
+  #2.現在還有幾件星際大戰Tshirt?
